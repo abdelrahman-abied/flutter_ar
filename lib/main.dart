62fdc62b5d8f11bcdeae550108d513036fed511b
abdelrahman-abied <abied_2011@yahoo.com>
2023-09-18 22:24:01 +0300

    add screen shot widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ar/screen_shot.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 import 'home.dart';
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ObjectGesturesWidget(),
+      home: ScreenshotWidget(),
     );
   }
 }
